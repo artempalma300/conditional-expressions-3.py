c3852971857b0f721ec6b4fd27f273031a8c61ae
artempalma300 <artempalma90@icloud.com>
2026-03-24 15:31:11 +0100

    Task 3

---
+++ type casting.py
@@ -0,0 +1,75 @@
+a = 2
+b = 2.5
+c = a + b
+print(c)
+
+a = "2"
+b = 3
+c = int(a) + b
+print(c)
+
+
+b = int(7.9)
+print(b)
+
+
+a = "25"
+b = int ("25") + 10
+print(b)
+
+
+a = 15
+message = "number: " + str(a)
+print(message)
+
+
+a = "5"
+b = 6
+c = int(a) + b
+print(c)
+
+
+a = int(True)
+print(a)
+
+
+
+a = "3.5"
+b = float(a) + 2
+print(b)
+
+
+
+a = 5
+b = 2
+c = float(a) / b
+print(c)
+
+
+age = input("age: ")
+age = int(age)
+future_age = age + 5
+print (f"in 5 years i will be: {future_age}")
+
+
+
+age = 20
+message = "your age: " + str(age)
+print(message)
+
+
+a = 3.4
+b = int(a) + 10
+print (b)
+
+
+
+name = input("hello, your name: ")
+age = 20
+message = f"your name: {name}, age: {age}"
+print(message)
+
+
+a = input("enter the number: ")
+a = int(a)
+print(a + 3)
